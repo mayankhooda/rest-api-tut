9ce63228e185784f7be0054f978674a9e8ca8dfd
Mayank Hooda <mayanknarenderhooda@gmail.com>
2019-03-16 16:27:37 +0530

    Initial commit

---
+++ src/main/java/com/mhooda/restapitut/RestapitutApplication.java
@@ -0,0 +1,13 @@
+package com.mhooda.restapitut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapitutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapitutApplication.class, args);
+	}
+
+}
